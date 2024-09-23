c6f9634b25772090579ab1f7be1604aa6dd75fe0
ArtemisNyx3 <artemisnyx3@yahoo.com>
2024-09-23 16:58:56 -0400

    game over logic

--- app.js
+++ app.js
@@ -3,46 +3,64 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameWindow = document.querySelector('.game_window');
 
   let birbBottom = 275;
-  let birbLeft = 175;
   gravity = 2;
-  let birbInterval = setInterval(gameStart, 15);
+  let game = false;
+  let gameInterval = setInterval(gameStart, 15);
 
-  addEventListener('click', fly);
+  document.addEventListener('click', fly);
   generatePipes();
   // console.log('appended chid');
 
   function gameStart() {
     birbBottom -= 2;
+
     birb.style.bottom = birbBottom + 'px';
-    birb.style.left = birbLeft + 'px';
   }
   function fly() {
-    birbBottom += 55;
+    birbBottom += 60;
     birb.style.bottom = birbBottom + 'px';
   }
   function generatePipes() {
     let pipeLeft = 400;
     let pipeHeight = Math.random() * 300;
     let pipeBottom = 0;
+    let pipeWidth = 60;
+
     const pipe = document.createElement('div');
     pipe.classList.add('pipes');
-    gameWindow.appendChild(pipe);
+    if (!game) gameWindow.appendChild(pipe);
     pipe.style.left = pipeLeft + 'px';
     pipe.style.bottom = pipeBottom + 'px';
     pipe.style.height = pipeHeight + 'px';
+
     // console.log('appended chid');
-    console.log(pipe);
+    //console.log(pipe);
+
     function movePipe() {
       pipeLeft -= 2;
       // console.log('PipeLeft = ' + pipeLeft);
       pipe.style.left = pipeLeft + 'px';
       if (pipeLeft === -50) {
-        console.log('Here');
         clearInterval(pipeInterval);
         gameWindow.removeChild(pipe);
       }
+
+      //game over conditions
+      //if (birbBottom <= pipeHeight - 30) gameOver();
+      if (birbBottom <= 0) {
+        gameOver();
+        clearInterval(pipeInterval);
+      }
     }
+
     let pipeInterval = setInterval(movePipe, 15);
-    setTimeout(generatePipes, 3000);
+    if (!game) setTimeout(generatePipes, 3000);
+  }
+
+  function gameOver() {
+    clearInterval(gameInterval);
+    game = true;
+    document.removeEventListener('click', fly);
+    console.log('gameOver');
   }
 });
