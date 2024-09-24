ffd00dc7661c23711bda90916eadf0a0c870c6d1
ArtemisNyx3 <artemisnyx3@yahoo.com>
2024-09-24 05:02:21 -0400

    start fresh

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-function startGame() {
-  const startbtn = document.querySelector('.startbtn');
+// import { Birb } from './birb';
+
+// // window.addEventListener('load', function () {
+// console.log('Loaded');
+// const canvas = document.querySelector('.gameWindow');
+// const ctx = canvas.getContext('2d');
+
+// class Game {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//     this.birb = new Birb(this);
+//   }
+//   update() {}
+//   draw() {
+//     this.birb.draw(ctx);
+//   }
+// }
+
+// const game = new Game(400, 600);
+// console.log(game);
+// // });
+function gameStart() {
+  console.log('here');
 }
